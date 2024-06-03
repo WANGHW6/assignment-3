d972dd2e5c8fec0761804f67870602e07b7b55e2
WANGHW6 <161697259+WANGHW6@users.noreply.github.com>
2024-06-03 22:12:39 +0800

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,14 +1,14 @@
 function toggleCart() {
   const button = document.querySelector('.add-to-cart button');
-  const cartIcon = document.querySelector('a[href="../../html/payment/view-cart.html"] img');
+  const cartIcon = document.querySelector('a[href="/assignment3/html/payment/view-cart.html"] img');
 
   if (button.classList.contains('added')) {
-      button.innerHTML = '<a>Add to Cart <img src="../../assets/icons/cart-white.svg" alt="Cart"></a>';
+      button.innerHTML = '<a>Add to Cart <img src="/assignment3/assets/icons/cart-white.svg" alt="Cart"></a>';
       button.classList.remove('added');
-      cartIcon.src = "/assets/icons/cart.svg";
+      cartIcon.src = "/assignment3/assets/icons/cart.svg";
   } else {
       button.innerHTML = '<a>Added</a>';
       button.classList.add('added');
-      cartIcon.src = "/assets/icons/cart-marked.svg";
+      cartIcon.src = "/assignment3/assets/icons/cart-marked.svg";
   }
 }
