d2238dcffd69c353ebc8092279f28f1ee7331db7
Haoyu Wang <wanghw6@student.unimelb.edu.au>
2024-06-03 20:43:10 +1000

    fix some problems--final submition

--- js/cart.js
+++ js/cart.js
@@ -1,9 +1,9 @@
 function toggleCart() {
   const button = document.querySelector('.add-to-cart button');
-  const cartIcon = document.querySelector('a[href="/html/payment/view-cart.html"] img');
+  const cartIcon = document.querySelector('a[href="../../html/payment/view-cart.html"] img');
 
   if (button.classList.contains('added')) {
-      button.innerHTML = '<a>Add to Cart <img src="/assets/icons/cart-white.svg" alt="Cart"></a>';
+      button.innerHTML = '<a>Add to Cart <img src="../../assets/icons/cart-white.svg" alt="Cart"></a>';
       button.classList.remove('added');
       cartIcon.src = "/assets/icons/cart.svg";
   } else {
